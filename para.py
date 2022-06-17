749829e2497a52e3c6b34b92eb8dda0b1c2e87a4
hqin-2020 <uctphqi@ucl.ac.uk>
2022-06-17 10:47:23 -0500

    init

--- para.py
+++ para.py
@@ -6,17 +6,18 @@ import seaborn as sns
 import pickle
 import os
 import time
+from scipy.stats import qmc
 np.set_printoptions(suppress = True)
-import sobol
+
 from Minimization import minimization
 from concurrent.futures import ProcessPoolExecutor
 obs_series = pd.read_csv('data.csv', delimiter=',')
 obs_series = np.array(obs_series.iloc[:,1:]).T
 
-n_points = 140000
-sobol_sequence = sobol.sample(dimension=22, n_points=n_points)
+sampler = qmc.Sobol(d=22, scramble = False)
+sobol_sequence = sampler.random_base2(m = 10)
 sobol_sequence[:,1:] = sobol_sequence[:,1:]*2 -1
-start = np.array_split(sobol_sequence, n_points)
+start = np.array_split(sobol_sequence, sobol_sequence.shape[0])
 
 if __name__ == '__main__':
     with ProcessPoolExecutor() as pool:
