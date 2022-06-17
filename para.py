83e34b21683a96ef49566cfde00ff0094a154ba9
hqin-2020 <uctphqi@ucl.ac.uk>
2022-06-17 10:51:02 -0500

    init

--- para.py
+++ para.py
@@ -6,7 +6,6 @@ import seaborn as sns
 import pickle
 import os
 import time
-from scipy.stats import qmc
 np.set_printoptions(suppress = True)
 
 from Minimization import minimization
@@ -14,7 +13,7 @@ from concurrent.futures import ProcessPoolExecutor
 obs_series = pd.read_csv('data.csv', delimiter=',')
 obs_series = np.array(obs_series.iloc[:,1:]).T
 
-sampler = qmc.Sobol(d=22, scramble = False)
+sampler = sp.stats.qmc.Sobol(d=22, scramble = False)
 sobol_sequence = sampler.random_base2(m = 10)
 sobol_sequence[:,1:] = sobol_sequence[:,1:]*2 -1
 start = np.array_split(sobol_sequence, sobol_sequence.shape[0])
